653dbc44fabd2a7f270817d13e210ed2d169cd7d
ErnestoDPuto31 <ernest.agalo31@gmail.com>
2026-01-23 18:30:16 +0800

    Week 3 Complete :)

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,123 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtBookName = new TextBox();
+            comboCategory = new ComboBox();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            txtAuthor = new TextBox();
+            btnEnterInfo = new Button();
+            txtUnique = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // txtBookName
+            // 
+            txtBookName.Location = new Point(55, 55);
+            txtBookName.Name = "txtBookName";
+            txtBookName.PlaceholderText = "Enter Book Name:";
+            txtBookName.Size = new Size(223, 27);
+            txtBookName.TabIndex = 1;
+            // 
+            // comboCategory
+            // 
+            comboCategory.AllowDrop = true;
+            comboCategory.FormattingEnabled = true;
+            comboCategory.Items.AddRange(new object[] { "Book", "Magazine", "E-Book", "Textbook", "Audiobook" });
+            comboCategory.Location = new Point(55, 153);
+            comboCategory.Name = "comboCategory";
+            comboCategory.Size = new Size(223, 28);
+            comboCategory.TabIndex = 2;
+            comboCategory.Text = "Enter Category:";
+            comboCategory.SelectedIndexChanged += comboCategory_SelectedIndexChanged;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(343, 99);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(404, 204);
+            listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 4;
+            // 
+            // txtAuthor
+            // 
+            txtAuthor.Location = new Point(55, 103);
+            txtAuthor.Name = "txtAuthor";
+            txtAuthor.PlaceholderText = "Enter Author:";
+            txtAuthor.Size = new Size(223, 27);
+            txtAuthor.TabIndex = 5;
+            // 
+            // btnEnterInfo
+            // 
+            btnEnterInfo.Location = new Point(55, 258);
+            btnEnterInfo.Name = "btnEnterInfo";
+            btnEnterInfo.Size = new Size(223, 45);
+            btnEnterInfo.TabIndex = 6;
+            btnEnterInfo.Text = "ENTER";
+            btnEnterInfo.UseVisualStyleBackColor = true;
+            btnEnterInfo.Click += btnEnterInfo_Click;
+            // 
+            // txtUnique
+            // 
+            txtUnique.Location = new Point(55, 208);
+            txtUnique.Name = "txtUnique";
+            txtUnique.Size = new Size(223, 27);
+            txtUnique.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(343, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 23);
+            label2.TabIndex = 8;
+            label2.Text = "LOG SHEET";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(txtUnique);
+            Controls.Add(btnEnterInfo);
+            Controls.Add(txtAuthor);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(comboCategory);
+            Controls.Add(txtBookName);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+        private TextBox txtBookName;
+        private ListBox listBox1;
+        private Label label1;
+        private TextBox txtAuthor;
+        private ComboBox comboCategory;
+        private Button btnEnterInfo;
+        private TextBox txtUnique;
+        private Label label2;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,184 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        private List<Book> bookCollection = new List<Book>();
+        public Form1()
+        {
+            InitializeComponent();
+            txtUnique.Enabled = false;
+        }
+
+        public class Book
+        {
+            public string? Title { get; set; }
+            public string? Author { get; set; }
+            public virtual string Category => "Book";
+
+            public virtual string GetInfo()
+            {
+                return $"Category: {Category}\nTitle: {Title}\nAuthor: {Author}";
+            }
+        }
+
+        // Derived classes
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string Category => "Magazine";
+            public override string GetInfo()
+            {
+                return $"{base.GetInfo()}\nIssue Number: {IssueNumber}";
+            }
+        }
+        public class EBook : Book
+        {
+            public int FileSizeMB { get; set; }
+            public override string Category => "E-Book";
+
+            public override string GetInfo()
+            {
+                return $"{base.GetInfo()}\nFile Size: {FileSizeMB} MB";
+            }
+        }
+        public class TextBook : Book
+        {
+            public string? Subject { get; set; }
+            public override string Category => "Textbook";
+
+            public override string GetInfo()
+            {
+                return $"{base.GetInfo()}\nSubject: {Subject}";
+            }
+        }
+        public class AudioBook : Book
+        {
+            public int DurationMinutes { get; set; }
+            public override string Category => "Audiobook";
+
+            public override string GetInfo()
+            {
+                return $"{base.GetInfo()}\nDuration: {DurationMinutes} minutes";
+            }
+        }
+
+
+
+        // METHOD TO DISPLAY BOOKS IN LISTBOX
+        private void DisplayBooks(Book[] books)
+        {
+            listBox1.Items.Clear();
+
+            foreach (var book in books)
+            {
+                String[] lines = book.GetInfo().Split('\n');
+                foreach (var line in lines)
+                {
+                    listBox1.Items.Add(line);
+                }
+                listBox1.Items.Add("----------------------------");
+            }
+        }
+
+        private void btnEnterInfo_Click(object sender, EventArgs e)
+        {
+            // ADD BOOK INFO TO LISTBOX
+            string category = comboCategory.SelectedItem?.ToString() ?? "";
+            try
+            {
+                switch (category)
+                {
+                    case "Book":
+                        var book = new Book
+                        {
+                            Title = txtBookName.Text,
+                            Author = txtAuthor.Text
+                        };
+                        bookCollection.Add(book);
+                        break;
+                    case "Magazine":
+                        var magazine = new Magazine
+                        {
+                            Title = txtBookName.Text,
+                            Author = txtAuthor.Text,
+                            IssueNumber = int.Parse(txtUnique.Text)
+                        };
+                        bookCollection.Add(magazine);
+                        break;
+                    case "E-Book":
+                        var ebook = new EBook
+                        {
+                            Title = txtBookName.Text,
+                            Author = txtAuthor.Text,
+                            FileSizeMB = int.Parse(txtUnique.Text)
+                        };
+                        bookCollection.Add(ebook);
+                        break;
+                    case "Textbook":
+                        var textbook = new TextBook
+                        {
+                            Title = txtBookName.Text,
+                            Author = txtAuthor.Text,
+                            Subject = txtUnique.Text
+                        };
+                        bookCollection.Add(textbook);
+                        break;
+                    case "Audiobook":
+                        var audiobook = new AudioBook
+                        {
+                            Title = txtBookName.Text,
+                            Author = txtAuthor.Text,
+                            DurationMinutes = int.Parse(txtUnique.Text)
+                        };
+                        bookCollection.Add(audiobook);
+                        break;
+                }
+
+            } catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid data for the unique field.");
+                return;
+            }
+
+            DisplayBooks(bookCollection.ToArray());
+
+            // CLEAR INPUT FIELDS
+            txtAuthor.Clear();
+            txtBookName.Clear();
+            txtUnique.Clear();
+            txtUnique.Enabled = false;
+            comboCategory.SelectedIndex = -1;
+            comboCategory.Text = "Select Category:";
+        }
+
+        private void comboCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string category = comboCategory.SelectedItem?.ToString() ?? "";
+            txtUnique.Clear();
+            txtUnique.Enabled = true;
+            switch (category)
+            {
+                case "Magazine":
+                    txtUnique.PlaceholderText = "Enter Issue Number:";
+                    txtUnique.Enabled = true;
+                    break;
+                case "E-Book":
+                    txtUnique.PlaceholderText = "Enter File Size (MB):";
+                    txtUnique.Enabled = true;
+                    break;
+                case "Textbook":
+                    txtUnique.PlaceholderText = "Enter Subject:";
+                    txtUnique.Enabled = true;
+                    break;
+                case "Audiobook":
+                    txtUnique.PlaceholderText = "Enter Duration (minutes):";
+                    txtUnique.Enabled = true;
+                    break;
+                default:
+                    txtUnique.Clear();
+                    txtUnique.Enabled = false;
+                    break;
+            }
+        }
+    }
 }
