b499f05d6c323219d5b591adbcd0870698790398
ErnestoDPuto31 <ernest.agalo31@gmail.com>
2026-03-04 09:43:17 +0800

    Week 9 Complete

--- lab9/Form1.Designer.cs
+++ lab9/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace lab9
 {
-	partial class Form1
+	partial class DataMining
 	{
 		/// <summary>
 		///  Required designer variable.
@@ -20,20 +20,240 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            txtSearchTitle = new TextBox();
+            btnSearchBooks = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            chartBookCategories = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label3 = new Label();
+            txtSearchQuery = new TextBox();
+            btnShowYearTrends = new Button();
+            chartYearTrends = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            chartAuthors = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label4 = new Label();
+            chartRatings = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)chartBookCategories).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chartYearTrends).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chartAuthors).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chartRatings).BeginInit();
+            SuspendLayout();
+            // 
+            // txtSearchTitle
+            // 
+            txtSearchTitle.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSearchTitle.Location = new Point(49, 67);
+            txtSearchTitle.Name = "txtSearchTitle";
+            txtSearchTitle.Size = new Size(375, 30);
+            txtSearchTitle.TabIndex = 0;
+            // 
+            // btnSearchBooks
+            // 
+            btnSearchBooks.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearchBooks.Location = new Point(49, 113);
+            btnSearchBooks.Name = "btnSearchBooks";
+            btnSearchBooks.Size = new Size(375, 47);
+            btnSearchBooks.TabIndex = 1;
+            btnSearchBooks.Text = "Search for Categories";
+            btnSearchBooks.UseVisualStyleBackColor = true;
+            btnSearchBooks.Click += btnSearchBooks_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(49, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Book TItle";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(49, 203);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 23);
+            label2.TabIndex = 4;
+            label2.Text = "Book TItle";
+            // 
+            // chartBookCategories
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartBookCategories.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chartBookCategories.Legends.Add(legend1);
+            chartBookCategories.Location = new Point(49, 188);
+            chartBookCategories.Name = "chartBookCategories";
+            chartBookCategories.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chartBookCategories.Series.Add(series1);
+            chartBookCategories.Size = new Size(454, 270);
+            chartBookCategories.TabIndex = 3;
+            chartBookCategories.Text = "chart1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(553, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 23);
+            label3.TabIndex = 4;
+            label3.Text = "Search Query";
+            // 
+            // txtSearchQuery
+            // 
+            txtSearchQuery.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSearchQuery.Location = new Point(553, 67);
+            txtSearchQuery.Name = "txtSearchQuery";
+            txtSearchQuery.Size = new Size(375, 30);
+            txtSearchQuery.TabIndex = 5;
+            // 
+            // btnShowYearTrends
+            // 
+            btnShowYearTrends.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnShowYearTrends.Location = new Point(553, 113);
+            btnShowYearTrends.Name = "btnShowYearTrends";
+            btnShowYearTrends.Size = new Size(375, 47);
+            btnShowYearTrends.TabIndex = 6;
+            btnShowYearTrends.Text = "Show Year Trends";
+            btnShowYearTrends.UseVisualStyleBackColor = true;
+            btnShowYearTrends.Click += btnShowYearTrends_Click;
+            // 
+            // chartYearTrends
+            // 
+            chartArea2.Name = "ChartArea1";
+            chartYearTrends.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            chartYearTrends.Legends.Add(legend2);
+            chartYearTrends.Location = new Point(553, 188);
+            chartYearTrends.Name = "chartYearTrends";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            chartYearTrends.Series.Add(series2);
+            chartYearTrends.Size = new Size(454, 270);
+            chartYearTrends.TabIndex = 7;
+            chartYearTrends.Text = "chart2";
+            // 
+            // chartAuthors
+            // 
+            chartArea3.Name = "ChartArea1";
+            chartAuthors.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            chartAuthors.Legends.Add(legend3);
+            chartAuthors.Location = new Point(49, 525);
+            chartAuthors.Name = "chartAuthors";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            chartAuthors.Series.Add(series3);
+            chartAuthors.Size = new Size(454, 284);
+            chartAuthors.TabIndex = 8;
+            chartAuthors.Text = "chart1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(49, 494);
+            label4.Name = "label4";
+            label4.Size = new Size(195, 23);
+            label4.TabIndex = 9;
+            label4.Text = "Most Frequent Authors";
+            // 
+            // chartRatings
+            // 
+            chartArea4.Name = "ChartArea1";
+            chartRatings.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            chartRatings.Legends.Add(legend4);
+            chartRatings.Location = new Point(553, 525);
+            chartRatings.Name = "chartRatings";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            chartRatings.Series.Add(series4);
+            chartRatings.Size = new Size(454, 284);
+            chartRatings.TabIndex = 10;
+            chartRatings.Text = "chart1";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(553, 494);
+            label5.Name = "label5";
+            label5.Size = new Size(141, 28);
+            label5.TabIndex = 11;
+            label5.Text = "Average Ratings";
+            label5.UseCompatibleTextRendering = true;
+            // 
+            // DataMining
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1062, 853);
+            Controls.Add(label5);
+            Controls.Add(chartRatings);
+            Controls.Add(label4);
+            Controls.Add(chartAuthors);
+            Controls.Add(chartYearTrends);
+            Controls.Add(btnShowYearTrends);
+            Controls.Add(txtSearchQuery);
+            Controls.Add(label3);
+            Controls.Add(chartBookCategories);
+            Controls.Add(label1);
+            Controls.Add(btnSearchBooks);
+            Controls.Add(txtSearchTitle);
+            Name = "DataMining";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)chartBookCategories).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartYearTrends).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartAuthors).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartRatings).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+        private Button btnSearchBooks;
+        private Label label1;
+        private Label label2;
+        private TextBox txtSearchTitle;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBookCategories;
+        private Label label3;
+        private TextBox txtSearchQuery;
+        private Button btnShowYearTrends;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartYearTrends;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartAuthors;
+        private Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRatings;
+        private Label label5;
+    }
 }
--- lab9/Form1.cs
+++ lab9/Form1.cs
@@ -1,10 +1,217 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing; // Needed for Colors
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace lab9
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class DataMining : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private const string apiKey = "AIzaSyA-Qlft8vRZnNLylP4kJiLL-zHHIMkOE2A";
+
+        public DataMining()
+        {
+            InitializeComponent();
+            ApplyProfessionalStyles();
+        }
+
+        private void ApplyProfessionalStyles()
+        {
+            // List all your charts here
+            Chart[] charts = { chartBookCategories, chartYearTrends, chartAuthors, chartRatings };
+
+            foreach (var chart in charts)
+            {
+                chart.BackColor = Color.White;
+                chart.ChartAreas[0].BackColor = Color.White;
+                chart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+                chart.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+                chart.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+                if (chart.Titles.Count == 0)
+                {
+                    chart.Titles.Add("Chart Title"); 
+                }
+
+                chart.Titles[0].Font = new Font("Segoe UI", 10, FontStyle.Bold);
+                chart.Titles[0].ForeColor = Color.DarkSlateGray;
+            }
+        }
+        private async void btnSearchBooks_Click(object sender, EventArgs e)
+        {
+            string query = txtSearchTitle.Text.Trim();
+            if (string.IsNullOrEmpty(query)) return;
+            var books = await FetchBooksDataAsync(query, "relevance");
+            if (books != null) VisualizeCategories(books);
+        }
+        private async void btnShowYearTrends_Click(object sender, EventArgs e)
+        {
+            string query = txtSearchQuery.Text.Trim();
+            if (string.IsNullOrEmpty(query)) return;
+
+            var books = await FetchBooksDataAsync(query, "relevance");
+
+            if (books != null)
+            {
+                VisualizeYears(books);
+                VisualizeAuthors(books);
+                VisualizeRatings(books);
+            }
+        }
+
+        private async Task<JArray> FetchBooksDataAsync(string query, string orderBy)
+        {
+            string apiUrl = $"https://www.googleapis.com/books/v1/volumes?q={query}&orderBy={orderBy}&maxResults=40&key={apiKey}";
+            try
+            {
+                var response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                var json = await response.Content.ReadAsStringAsync();
+                return (JArray)JObject.Parse(json)["items"];
+            }
+            catch
+            {
+                MessageBox.Show("No data found or API limit reached.");
+                return null;
+            }
+        }
+
+        // PIE CHART
+        private void VisualizeCategories(JArray books)
+        {
+            var stats = new Dictionary<string, int>();
+
+            foreach (var book in books)
+            {
+                var cats = book["volumeInfo"]?["categories"];
+                if (cats != null)
+                {
+                    string cat = cats[0].ToString();
+                    if (stats.ContainsKey(cat)) stats[cat]++; else stats[cat] = 1;
+                }
+            }
+
+            // Group small items into "Others"
+            var finalStats = new Dictionary<string, int>();
+            int others = 0;
+            foreach (var kvp in stats)
+            {
+                if (kvp.Value < 2) others += kvp.Value;
+                else finalStats.Add(kvp.Key, kvp.Value);
+            }
+            if (others > 0) finalStats.Add("Others", others);
+
+            chartBookCategories.Series.Clear();
+            Series s = chartBookCategories.Series.Add("Categories");
+            s.ChartType = SeriesChartType.Pie;
+
+            s["PieLabelStyle"] = "Outside";
+            s.BorderColor = Color.White;
+            s.BorderWidth = 1;
+
+            foreach (var pair in finalStats)
+            {
+                int p = s.Points.AddXY(pair.Key, pair.Value);
+                s.Points[p].LegendText = pair.Key; 
+                s.Points[p].Label = "#VAL";        
+            }
+
+            chartBookCategories.Legends[0].Docking = Docking.Bottom;
+            chartBookCategories.Legends[0].Alignment = StringAlignment.Center;
+        }
+
+        // YEARS CHART
+        private void VisualizeYears(JArray books)
+        {
+            var stats = new SortedDictionary<string, int>();
+
+            foreach (var book in books)
+            {
+                string date = book["volumeInfo"]?["publishedDate"]?.ToString();
+                if (!string.IsNullOrEmpty(date) && date.Length >= 4)
+                {
+                    string year = date.Substring(0, 4);
+                    if (stats.ContainsKey(year)) stats[year]++; else stats[year] = 1;
+                }
+            }
+
+            chartYearTrends.Series.Clear();
+            Series s = chartYearTrends.Series.Add("Years");
+            s.ChartType = SeriesChartType.Column;
+            s.Color = Color.CornflowerBlue; 
+
+            foreach (var pair in stats) s.Points.AddXY(pair.Key, pair.Value);
+        }
+
+        // AUTHORS CHART
+        private void VisualizeAuthors(JArray books)
+        {
+            var stats = new Dictionary<string, int>();
+
+            foreach (var book in books)
+            {
+                var authors = book["volumeInfo"]?["authors"];
+                if (authors != null)
+                {
+                    foreach (var auth in authors)
+                    {
+                        string name = auth.ToString();
+                        // Shorten long names
+                        if (name.Length > 18) name = name.Substring(0, 15) + "...";
+                        if (stats.ContainsKey(name)) stats[name]++; else stats[name] = 1;
+                    }
+                }
+            }
+
+            var top = stats.OrderByDescending(x => x.Value).Take(5);
+
+            chartAuthors.Series.Clear();
+            Series s = chartAuthors.Series.Add("Authors");
+            s.ChartType = SeriesChartType.Bar;
+            s.Color = Color.SeaGreen;
+
+            foreach (var pair in top) s.Points.AddXY(pair.Key, pair.Value);
+        }
+
+        // RATINGS CHART
+        private void VisualizeRatings(JArray books)
+        {
+            int[] ratings = new int[6]; 
+
+            foreach (var book in books)
+            {
+                var rToken = book["volumeInfo"]?["averageRating"];
+                if (rToken != null)
+                {
+                    double val = (double)rToken;
+                    int r = (int)Math.Round(val);
+                    if (r >= 1 && r <= 5) ratings[r]++;
+                }
+            }
+
+            chartRatings.Series.Clear();
+            Series s = chartRatings.Series.Add("Ratings");
+            s.ChartType = SeriesChartType.Line;
+            s.BorderWidth = 3;
+            s.Color = Color.OrangeRed;
+            s.MarkerStyle = MarkerStyle.Circle;
+            s.MarkerSize = 8;
+            for (int i = 1; i <= 5; i++)
+            {
+                s.Points.AddXY(i, ratings[i]);
+            }
+
+            chartRatings.ChartAreas[0].AxisX.CustomLabels.Clear();
+            for (int i = 1; i <= 5; i++)
+            {
+                chartRatings.ChartAreas[0].AxisX.CustomLabels.Add(i - 0.5, i + 0.5, i + " ★");
+            }
+        }
+    }
 }
